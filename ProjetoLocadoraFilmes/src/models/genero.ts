d2e4db7d7388f1b2314031e367e1a43551a16a2d
Danilo Farias <dansoaresfarias@gmail.com>
2023-12-13 21:29:17 -0300

    Criando as rotas, contrllers, e repositorie

--- ProjetoLocadoraFilmes/src/models/genero.ts
+++ ProjetoLocadoraFilmes/src/models/genero.ts
@@ -1,30 +1,12 @@
-export class Genero{
+export class Genero {
 
     // Fields
-    private id: number;
-    private nome: string;
+    id?: number;
+    nome: string;
 
     // Constructor
     constructor(id: number, nome: string) {
         this.id = id;
-        this.nome = nome;
-    }
-
-    // Getters
-    getId(): number {
-        return this.id;
-    }
-
-    getNome(): string {
-        return this.nome;
-    }
-
-    // Setters
-    setId(id: number): void {
-        this.id = id;
-    }
-
-    setNome(nome: string): void {
         this.nome = nome;
     }
 }
--- ProjetoLocadoraFilmes/src/routes/routes.ts
+++ ProjetoLocadoraFilmes/src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Application } from "express";
 import homeRoutes from "./home.routes";
+import generoRoutes from "./genero.routes";
 
+// Concentrador de rotas
 export default class Routes {
   constructor(app: Application) {
     app.use("/alohaFilmes", homeRoutes);
-
+    app.use("/alohaFilmes", generoRoutes)
   }
 }
